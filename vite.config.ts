f8ff8e5e43dba27da8bb5cb93401951d59d528bc
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-07-23 08:56:51 -0500

    debugginh gh pages still

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: process.env.NODE_ENV === 'production' ? '/ecom-services/' : '/', // only use base path in production
-  build: {
+  base: '/ecom-services/',
+    build: {
     outDir: 'build/client',
     emptyOutDir: true,
   },
